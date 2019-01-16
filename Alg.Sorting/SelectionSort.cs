203e50ff29cc4ee7dafdfdb0f4702ee4cf6f49a5
胡永晖 <huyonghui@gaohangip.com>
2019-01-16 11:44:17 +0800

    Algorithm，选择排序

---
+++ Alg.Sorting/SelectionSort.cs
@@ -0,0 +1,25 @@
+﻿namespace Alg.Sorting
+{
+    public class SelectionSort : ISort
+    {
+        public void Execute(ref int[] arr)
+        {
+            for (int i = 0, len = arr.Length; i < len; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < len; j++)
+                    if (arr[min] > arr[j])
+                        min = j;
+
+                Swap(arr, min, i);
+            }
+        }
+
+        private void Swap(int[] arr, int i, int j)
+        {
+            var tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+}
