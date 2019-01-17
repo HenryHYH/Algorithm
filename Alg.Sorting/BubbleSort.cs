b1433599c070bb3f7861ea43570b0e82549455b2
胡永晖 <huyonghui@gaohangip.com>
2019-01-17 16:46:40 +0800

    Algorithm，冒泡排序

---
+++ Alg.Sorting/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Alg.Sorting
+{
+    public class BubbleSort : ISort
+    {
+        public void Execute(ref int[] arr)
+        {
+            var tmp = 0;
+            bool swapped;
+            for (int i = 0, iMax = arr.Length; i < iMax; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < iMax - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                        if (!swapped)
+                            swapped = true;
+                    }
+                }
+                if (!swapped)
+                    return;
+            }
+        }
+    }
+}
